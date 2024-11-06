fa77332c3bcc15695e471a228477af9bb1845793
SelmanEken <eken_selmann@hotmail.com>
2024-11-06 22:48:42 +0300

    Add project files.

---
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concrete
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+    }
+}
