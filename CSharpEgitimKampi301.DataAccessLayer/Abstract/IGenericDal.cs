da8df80f477872e2cf156453b7aac188231a08b4
SelmanEken <eken_selmann@hotmail.com>
2024-11-10 11:18:58 +0300

    .

---
+++ CSharpEgitimKampi301.DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace CSharpEgitimKampi301.DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T> where T : class
+    {
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(int id);
+        List<T> GetAll();
+        T GetById(int id);
+        
+    }
+}
